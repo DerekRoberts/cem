ae604a12fe7d08ab6dd3df9e0dd6c9e197591676
Derek Roberts <derek.roberts@gmail.com>
2020-08-14 17:36:13 -0700

    Add env_file support for run.sh

--- local-infrastructure/run.sh
+++ local-infrastructure/run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# 
+#
 # https://bcgov.github.io/common-forms-toolkit/local-infrastructure/
 #
 # Prerequisites: docker, docker compose and python
 #
-set -euxo pipefail
+set -euo pipefail
+
+# Default env file, can be overridden (VAR=something ./run.sh)
+#
+ENV_FILE="${ENV_FILE:-.env}"
+
+# Pick up vars from .env, if exists
+#
+[ ! -f "${ENV_FILE}" ] || source ./.env
+KEY_PORT=${KEYCLOAK_HOST_HTTP_PORT:-28080}
 
 # Stop, build and bring up containers as services (daemons)
 #
@@ -12,9 +21,15 @@ docker-compose stop
 docker-compose build
 docker-compose up -d
 
+# Follow migrations
+#
+docker logs -f comfort_node_migrate
+
 # Wait for keycloak to start and create local users
 #
-sleep 10
+while (! curl -s http://localhost:${KEY_PORT}/auth/realms/cp1qly2d -o /dev/null); do
+    sleep 10
+done
 docker exec -ti comfort_keycloak bash /tmp/keycloak-local-user.sh
 
 # Show status of containers
